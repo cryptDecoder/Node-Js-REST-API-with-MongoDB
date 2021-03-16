2172e58ceef4e7ad581be16293b318a74f876427
unknown <pruthvirajs2007@gmail.com>
2021-03-17 03:17:49 +0530

    Basic GET request perforemed

--- server.js
+++ server.js
@@ -15,3 +15,25 @@ app.use(bodyParser.json());
 app.use(bodyParser.urlencoded({ extended: true }));
 
 var port = process.env.PORT || 8080; // set port 8080 to execute out API
+
+// ROUTES FOR OUR API
+
+// ###############################################################################
+
+var router = express.Router();
+
+// test our route
+
+router.get("/", function (req, res) {
+  res.json({ message: "hooray! welcome to our REST API" });
+});
+
+// More routes will be added bellow here
+
+// REGISTER OUR ROUTES
+
+app.use("/api", router);
+
+// START THE SERVER
+app.listen(port);
+console.log("Something awesome magic happens on port " + port);
